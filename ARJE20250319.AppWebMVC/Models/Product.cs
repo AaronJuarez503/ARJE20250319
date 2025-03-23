6a9b6012c30a62bc6bd61f1d46e7d5e76133f81b
Aaron Juarez <Aaron Juarez@DESKTOP-DKTIVHI>
2025-03-22 19:54:09 -0600

    Retoques

--- ARJE20250319.AppWebMVC/Models/Product.cs
+++ ARJE20250319.AppWebMVC/Models/Product.cs
@@ -8,21 +8,28 @@ public partial class Product
 {
     public int ProductId { get; set; }
 
+    [Display(Name = "Producto")]
     [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string ProductName { get; set; } = null!;
 
+    [Display(Name = "Descripcion")]
     public string? Description { get; set; }
 
+    [Display(Name = "Precio")]
     [Required(ErrorMessage = "El Precio es obligatorio.")]
     [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números y puntos.")]
 
     public decimal Price { get; set; }
 
+    [Display(Name = "Marca")]
     public int? BrandId { get; set; }
 
+    [Display(Name = "Bodega")]
     public int? WarehouseId { get; set; }
 
+    [Display(Name = "Marca")]
     public virtual Brand? Brand { get; set; }
 
+    [Display(Name = "Bodega")]
     public virtual Warehouse? Warehouse { get; set; }
 }
--- ARJE20250319.AppWebMVC/Models/User.cs
+++ ARJE20250319.AppWebMVC/Models/User.cs
@@ -9,6 +9,7 @@ public partial class User
 {
     public int UserId { get; set; }
 
+    [Display(Name = "Usuario")]
     [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Username { get; set; } = null!;
 
@@ -16,17 +17,19 @@ public partial class User
     [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
     public string Email { get; set; } = null!;
 
-    [Required(ErrorMessage = "El password es obligatorio.")]
+    [Display(Name = "Contraseña")]
+    [Required(ErrorMessage = "La contraseña es obligatorio.")]
     [DataType(DataType.Password)]
-    [StringLength(40, MinimumLength = 5, ErrorMessage = "El passowrd debe tener entre 5 y 50 caracteres.")]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "La contraseña debe tener entre 5 y 50 caracteres.")]
     public string PasswordHash { get; set; } = null!;
 
+    [Display(Name = "Rol")]
     [Required(ErrorMessage = "El Rol es obligatorio.")]
     public string Role { get; set; } = null!;
 
     [NotMapped]
-    [StringLength(40, MinimumLength = 5, ErrorMessage = "El password debe tener entre 5 y 50 caracteres.")]
-    [Display(Name = "Confirmar Password")]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "La contraseña debe tener entre 5 y 50 caracteres.")]
+    [Display(Name = "Confirmar Contraseña")]
     [DataType(DataType.Password)]
     [Compare("PasswordHash", ErrorMessage = "Las contraseñas no coinciden.")]
     public string? ConfirmarPassword { get; set; } = null!;
